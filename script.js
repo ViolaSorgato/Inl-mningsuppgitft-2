542be07fcbe9e62658604c2441e204f62027c5fa
Viola Sorgato <violasorgato@gmail.com>
2022-10-07 11:27:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const menu = document.querySelector (".menu");
+const name = document.querySelector ("#name");
+const password = document.querySelector ("#password");
+const loginMessage = document.querySelector ("#loginmessage");
