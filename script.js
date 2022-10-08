c3b6410dadb61ec0a487098f06a97381777c6575
Viola Sorgato <violasorgato@gmail.com>
2022-10-08 19:27:13 +0200

    Another commit

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ const content = document.querySelector (".content");
 const wrongLogin = document.querySelector ("#wronglogin");
 const rightLogin = document.querySelector ("#rightlogin");
 
-logInBtn.addEventListener("click", myFunction(){
-    console.log("Hej!"),
-});
+const chosenUser = "fredrik";
+const chosenPassword = "12345";
+
+logInBtn.addEventListener ("click", checkInfo);
+
+function checkInfo() {
+    if (userName.value == chosenUser && password.value == chosenPassword) {
+        renderSuccessfulUI
+    } else {
+        renderFailUI()
+    }
+}
+
+renderSuccessfulUI() {
+    menu.style.display = "none";
+    rightLogin.style.display = "block";
+    rightLogin + "" + "Fredrik!";
+}
+
+renderFailUI() {
+    menu.style.display = "none";
+    wrongLogin.style.display = "block";
+}
+
+
